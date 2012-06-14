ab9be831117fbd065de0a017041e3c4a68980a48
Fred Condo <fcondo@quinn.com>
2012-06-14 13:17:01 -0700

    MINOR: Collect base translations
    - Ran the i18nTextCollectorTask

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,9 +2,18 @@
 
 global $lang;
 
+$lang['en_US']['DocumentationSearch']['SEARCH'] = 'Search';
 $lang['en_US']['DocumentationSearch']['SEARCHRESULTS'] = 'Search Results';
 $lang['en_US']['DocumentationViewer']['CHANGE'] = 'Change';
+$lang['en_US']['DocumentationViewer']['KEYWORDS'] = 'Keywords';
 $lang['en_US']['DocumentationViewer']['LANGUAGE'] = 'Language';
+$lang['en_US']['DocumentationViewer']['MODULES'] = 'Modules';
+$lang['en_US']['DocumentationViewer']['OPENSEARCHDESC'] = 'Search the documentation';
+$lang['en_US']['DocumentationViewer']['OPENSEARCHNAME'] = 'Documentation Search';
+$lang['en_US']['DocumentationViewer']['OPENSEARCHTAGS'] = 'documentation';
 $lang['en_US']['DocumentationViewer']['SEARCH'] = 'Search';
+$lang['en_US']['DocumentationViewer']['VERSIONS'] = 'Versions';
+$lang['en_US']['DocumentationViewer_home.ss']['DOCUMENTEDMODULES'] = 'Documented Modules';
+$lang['en_US']['DocumentationViewer_home.ss']['NOMODULEDOCUMENTATION'] = 'No modules with documentation installed could be found.';
+$lang['en_US']['DocumentationViewer_results.ss']['ADVANCEDSEARCH'] = 'Advanced Search';
 
-?>
